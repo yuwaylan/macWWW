41f8e173dd099f94620b834d8d56558f0a864b66
yuwaylan <0987431104a@gmail.com>
2020-11-09 17:52:06 +0800

    try to cast to base64

--- do_encode.py
+++ do_encode.py
@@ -1,11 +1,34 @@
 import os
 import base64
+from pathlib import Path
 
-from io import BytesIO
+file_path = "./up_load/"
+file_list = os.listdir(file_path)
 
-def str2base64pic(s:str):
-    img = qrcode.make(s)
-    buffered = BytesIO()
-    img.save(buffered, format="JPEG")
-    return base64.b64encode(buffered.getvalue()).decode("utf-8")
+DEBUG =True
+
+
+def _print(printItem):
+    if(DEBUG):
+        print(printItem)
+        
+
+for file in file_list :
+    if ".wav" in file or ".svg"in file:
+        with open(file_path + file,"rb") as f:
+            #### Data procress encode to base64
+            file_name = file[:-4]
+            _print(file_name)
+            base64_data = base64.b64encode(f.read())# base64.b64decode(base64data)
+            # _print(base64_data)
+            
+            #### Write to html 
+            b64_img_html = file_path + file_name + ".html"
+            Path(b64_img_html).touch()
+            src = '<img id="'+file_name+'" src="data:'+str(base64_data)+'"/>'
+            html = open(b64_img_html, mode='w')
+            html.write(src)
+            html.close()
+            _print("\r\n\r\n\r\n\r\n")
+    
 
